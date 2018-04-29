165bb0f5cc4b544d23ac501d6230fc11e1339255
JaktensTid <jaktenstid1@gmail.com>
2018-04-29 19:39:52 +0300

    Working scrolling

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,20 @@
 debugger;
+
+window.onload = function() {
+    var url = document.URL;
+    chrome.storage.local.get(url, function(result) {
+        window.scroll({'left': 0, 'top': result[url], 'behavior': 'smooth'});
+    });
+};
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    function(request) {
         if (request.action == 'getHeight'){
             $(document).one('mousemove', function(e){
-                sendResponse({'height': document.body.offsetHeight, 'Y': e.pageY});
+                var url = document.URL;
+                var result = {};
+                result[url] = e.pageY;
+                chrome.storage.local.set(result);
             });
             return true;
         }
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class App {
         chrome.contextMenus.onClicked.addListener(function (e){
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {action: "getHeight"}, function(response) {
-                    console.log(response);
+
                 });
             });
         });
